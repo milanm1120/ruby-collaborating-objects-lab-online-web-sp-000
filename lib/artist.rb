37f2f745a62aeaf077158f2add3d9b1ba0931e05
Milan Mody <milanm1120@gmail.com>
2021-03-18 21:41:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name, :song
   @@all = []
 
   def initialize(name)
@@ -11,8 +11,8 @@ class Artist
     @@all
   end
 
-  def add_song(songs)
-    songs.artist = self
+  def add_song(song)
+    song.artist = self
   end
 
 end

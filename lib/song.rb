d72d40b5cb66868ef409fb1c8bfda262940906a2
Milan Mody <milanm1120@gmail.com>
2021-03-18 22:10:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,9 @@ class Song
 
   def self.new_by_filename(filename)
     split_name = filename.split (" - ")
+    self.new(split_name[1])
+    artist = Artist.find_or_create_by_name(split_name[0])
+    song.artist = artist
   end
 
 end
